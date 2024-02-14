8995e59a8c7ccd6f9dca7611663ce91422ad2779
Eric <ericgeorge4477@gmail.com>
2024-02-15 01:55:10 +0200

    added container content and more styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import Accordion from "./components/Accordion";
 function App() {
+  const items = [
+    {
+      title: "What is Frontend Mentor, and how will it help me?",
+      content: `Frontend Mentor offers realistic coding challenges to help developers improve their 
+    frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
+    all levels and ideal for portfolio building.`,
+      id: "AQW1234",
+    },
+    {
+      title: "Is Frontend Mentor free?",
+      content: `Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
+    option providing access to a range of projects suitable for all skill levels.`,
+      id: "AQWSD1234",
+    },
+    {
+      title: " Can I use Frontend Mentor projects in my portfolio?",
+      content: ` Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
+    way to showcase your skills to potential employers!`,
+      id: "AQWWE1234",
+    },
+    {
+      title: "   How can I get help if I'm stuck on a challenge?",
+      content: `The best place to get help is inside Frontend Mentor's Discord community. There's a help 
+      channel where you can ask questions and seek support from other community members.`,
+      id: "AQWWEF1234",
+    },
+  ];
   return (
     <div className="app">
-      <Accordion />
+      <Accordion items={items} />
     </div>
   );
 }
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,5 +1,29 @@
-function Accordion() {
-  return <div className="accordion"></div>;
+function Accordion({ items }) {
+  const renderedItems = items.map((item) => {
+    return (
+      <div className="item" key={item.id}>
+        <div className="item-title">
+          <h3>{item.title}</h3>
+          <span>
+            <img src="/assets/images/icon-plus.svg" alt="icon-plus" />
+          </span>
+        </div>
+        <div className="item-content">
+          <p>{item.content}</p>
+        </div>
+        <hr />
+      </div>
+    );
+  });
+  return (
+    <div className="accordion">
+      <div className="accordion-header">
+        <img src="/assets/images/icon-star.svg" alt="icon-star" />
+        <h1>FAQs</h1>
+      </div>
+      <div className="accordion-items">{renderedItems}</div>
+    </div>
+  );
 }
 
 export default Accordion;
