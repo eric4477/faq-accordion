cc0a6bbab4e05d08f67200f2b46031a18e2d51fe
Eric <ericgeorge4477@gmail.com>
2024-02-15 23:44:14 +0200

    added functionality and more styles

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
+
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [itemIndex, setItemIndex] = useState(-1);
+
+  const handleClick = (index) => {
+    if (index === itemIndex) {
+      setItemIndex(-1);
+    } else {
+      setItemIndex(index);
+    }
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === itemIndex;
+    let icon = isExpanded
+      ? "../assets/images/icon-minus.svg"
+      : "../assets/images/icon-plus.svg";
+
+    let alt = isExpanded ? "icon-minus" : "icon-plus";
+
     return (
       <div className="item" key={item.id}>
-        <div className="item-title">
+        <div onClick={() => handleClick(index)} className="item-title">
           <h3>{item.title}</h3>
           <span>
-            <img src="/assets/images/icon-plus.svg" alt="icon-plus" />
+            <img src={icon} alt={alt} />
           </span>
         </div>
-        <div className="item-content">
-          <p>{item.content}</p>
-        </div>
+        {isExpanded && (
+          <div className="item-content">
+            <p>{item.content}</p>
+          </div>
+        )}
         <hr />
       </div>
     );
@@ -18,7 +39,9 @@ function Accordion({ items }) {
   return (
     <div className="accordion">
       <div className="accordion-header">
-        <img src="/assets/images/icon-star.svg" alt="icon-star" />
+        <span>
+          <img src="../assets/images/icon-star.svg" alt="icon-star" />
+        </span>
         <h1>FAQs</h1>
       </div>
       <div className="accordion-items">{renderedItems}</div>
